1a450c74eaf6a721d2a0c9f506ad6736b9c263ad
CodeDrip <dhaesha@gmail.com>
2019-11-26 09:16:30 -0500

    Still making progress

--- pixart.js
+++ pixart.js
@@ -1,1 +1,6 @@
+//target the element class brush
+console.log(document.querySelector(".brush"))
 
+//set click event for "set color" button
+//event handeler will extract information from input field
+//assign input value to "style.backgroundColor"
