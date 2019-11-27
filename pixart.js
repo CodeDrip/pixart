451bb5da46c3ea240ae51916a90857f7bf6ca8d4
CodeDrip <dhaesha@gmail.com>
2019-11-26 22:21:29 -0500

    div background color equals input

--- pixart.js
+++ pixart.js
@@ -1,6 +1,38 @@
 //target the element class brush
-console.log(document.querySelector(".brush"))
+// let colorBox = document.querySelector("div.brush")
+// console.log(colorBox)
 
-//set click event for "set color" button
+//form > button target
+
+let colorInput = document.querySelector("#form")
+console.log(colorInput)
+
+colorInput.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    document.querySelector(".brush").style.background 
+    = document.querySelector("#color-field").value;
+})
+
+// var artsy = document.createElement('div');
+// document.body.appendChild(artsy);
+// artsy.classList.add(artsy);
+
+
+
+
+
+
+    // document.querySelector(".brush").style.background = document.querySelector("#color-field").style.background;
+
+    // form.addEventListener("submit", function(evt){
+//     evt.preventDefault();
+//     alert("it works");
+//     })
+
 //event handeler will extract information from input field
 //assign input value to "style.backgroundColor"
+
+
+//check colorbox color
+// colorBox.style.backgroundColor
+// document.querySelector("div.brush").style.backgroundColor
