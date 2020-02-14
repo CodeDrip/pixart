42473fd6aa7d308284ed63d9d2a82fd79d952d9c
CodeDrip <dhaesha@gmail.com>
2020-02-14 09:36:00 -0500

    changed stuff

--- pixart.js
+++ pixart.js
@@ -1,3 +1,68 @@
+// //target the element class brush
+// // let colorBox = document.querySelector("div.brush")
+// // console.log(colorBox)
+
+// //form > button target
+
+// let colorInput = document.querySelector("#form")
+// console.log(colorInput)
+
+// colorInput.addEventListener("submit", function(evt){
+//     evt.preventDefault();
+//     document.querySelector(".brush").style.background 
+//     = document.querySelector("#color-field").value;
+// })
+
+// var i;
+// for(i=0; i < 8000; i++){
+//     var msPaintBox = document.createElement('div');
+//     msPaintBox.className = "square"
+//     document.getElementsByTagName('body')[0].appendChild(msPaintBox);
+//     msPaintBox.classList = "square"
+//     msPaintBox.addEventListener("mouseover", function(evt) {
+//         evt.preventDefault();
+//         evt.target.style.background 
+//         = document.querySelector("#color-field").value;
+//     })
+// }
+
+// // for(i=0; i < 20; i++){
+// //     document.querySelectorAll(".square").style.background 
+// //     = document.querySelector("#color-field").value;
+// // }    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // var artsy = document.createElement('div');
+// // document.body.appendChild(artsy);
+// // artsy.classList.add(artsy);
+
+
+
+
+
+
+//     // document.querySelector(".brush").style.background = document.querySelector("#color-field").style.background;
+
+//     // form.addEventListener("submit", function(evt){
+// //     evt.preventDefault();
+// //     alert("it works");
+// //     })
+
+// //event handeler will extract information from input field
+// //assign input value to "style.backgroundColor"
+
 //target the element class brush
 // let colorBox = document.querySelector("div.brush")
 // console.log(colorBox)
@@ -62,5 +127,4 @@ for(i=0; i < 8000; i++){
 
 //event handeler will extract information from input field
 //assign input value to "style.backgroundColor"
-
 
